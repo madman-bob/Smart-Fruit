441cc64613d52889c2589756e29d3452288e74ef
madman-bob <madman.bob@hotmail.co.uk>
2018-07-03 15:12:21 +0100

    Refactor setup.py file openings into functions

--- setup.py
+++ setup.py
@@ -5,23 +5,28 @@ import smart_fruit
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
-    long_description = readme_file.read()
 
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as requirements_file:
-    requirements = [requirement.strip() for requirement in requirements_file if requirement.strip()]
+def get_long_description():
+    with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
+        return readme_file.read()
+
+
+def get_requirements():
+    with open(path.join(here, 'requirements.txt'), encoding='utf-8') as requirements_file:
+        return [requirement.strip() for requirement in requirements_file if requirement.strip()]
+
 
 setup(
     name='smart-fruit',
     version=smart_fruit.__version__,
     packages=['smart_fruit'],
-    install_requires=requirements,
+    install_requires=get_requirements(),
 
     author='Robert Wright',
     author_email='madman.bob@hotmail.co.uk',
 
     description='A Python schema-based machine learning library',
-    long_description=long_description,
+    long_description=get_long_description(),
     long_description_content_type='text/x-rst',
     url='https://github.com/madman-bob/Smart-Fruit',
     license='MIT',
