af3b551877f1a80d9759e769ee74b605bdf5215f
madman-bob <madman.bob@hotmail.co.uk>
2018-07-12 14:38:32 +0100

    Add custom model class test

--- tests/test_model_training.py
+++ tests/test_model_training.py
@@ -3,14 +3,16 @@ from unittest import TestCase
 
 from numpy import median
 
+from sklearn import linear_model
+
 from examples.trivial_model import TrivialModel
 
 
 class TestModelTraining(TestCase):
     @staticmethod
-    def _train_test_split(func, **kwargs):
-        model, score = TrivialModel.train(
-            TrivialModel.features_from_list((n, func(n)) for n in range(20)),
+    def _train_test_split(func, model_class=TrivialModel, **kwargs):
+        model, score = model_class.train(
+            model_class.features_from_list((n, func(n)) for n in range(20)),
             **kwargs
         )
 
@@ -59,3 +61,22 @@ class TestModelTraining(TestCase):
             with self.subTest(test_sample_count=test_sample_count), \
                  self.assertRaises(ValueError):
                 self._train_test_split(lambda n: 10 * n, test_sample_count=test_sample_count)
+
+    def test_custom_model_class(self):
+        class HuberModel(TrivialModel):
+            model_class = linear_model.HuberRegressor
+
+        almost_straight_line = lambda n: 10 * n if n else 10
+
+        self.assertLess(
+            self._median_train_test_split(
+                almost_straight_line,
+                model_class=TrivialModel,
+                train_test_split_ratio=0.2
+            ),
+            self._median_train_test_split(
+                almost_straight_line,
+                model_class=HuberModel,
+                train_test_split_ratio=0.2,
+            )
+        )
