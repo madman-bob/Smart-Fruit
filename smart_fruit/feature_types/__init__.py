7551928cd147571ac61ffe73f77e2c88eec6a04a
madman-bob <madman.bob@hotmail.co.uk>
2018-07-17 16:48:55 +0100

    Split feature_types module into multiple files

---
+++ smart_fruit/feature_types/__init__.py
@@ -0,0 +1,4 @@
+from smart_fruit.feature_types.feature_type_base import FeatureType
+from smart_fruit.feature_types.simple_types import Number, Label
+
+__all__ = ["FeatureType", "Number", "Label"]
--- smart_fruit/feature_types/simple_types.py
+++ smart_fruit/feature_types/simple_types.py
@@ -1,37 +1,11 @@
-from abc import ABCMeta
 from collections import namedtuple
 
 from numpy import isfinite
 from pandas import Series
 
-__all__ = ["FeatureType", "Number", "Label"]
+from smart_fruit.feature_types.feature_type_base import FeatureType
 
-
-class FeatureType(metaclass=ABCMeta):
-    _index = None
-    feature_count = 1
-
-    def __get__(self, instance, owner):
-        if instance is None:
-            return self
-
-        if self._index is None:
-            self._index = next(
-                i
-                for i, name in enumerate(owner._fields)
-                if getattr(owner, name) is self
-            )
-
-        return instance[self._index]
-
-    def validate(self, value):
-        return value
-
-    def to_series(self, value):
-        return Series([value])
-
-    def from_series(self, features):
-        return features[0]
+__all__ = ["Number", "Label"]
 
 
 class Number(FeatureType):
